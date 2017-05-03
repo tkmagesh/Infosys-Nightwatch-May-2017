a53a6cf3ac7f578d6ed4b9fc8278a204f21760ad
Magesh <tkmagesh77@gmail.com>
2017-05-03 15:14:06 +0530

    async - part - 1

--- Day-01/01-JavaScript/async.js
+++ Day-01/01-JavaScript/async.js
@@ -12,7 +12,23 @@ var app = (function(){
 		console.log('[@Client] result = ', result);
 	}
 
+	function addAsync(x,y){
+		console.log('	[@Service] processing ', x , ' and ', y);
+		setTimeout(function(){
+			var result = x + y;
+			console.log('	[@Service] returing result ');
+			return result;
+		}, 5000);
+	}
+
+	function addAsyncClient(x,y){
+		console.log('[@Client] triggering addAsync');
+		var result = addAsync(x,y);
+		console.log('[@Client] result = ', result);
+	}
+
 	return {
-		addSyncClient : addSyncClient
+		addSyncClient : addSyncClient,
+		addAsyncClient : addAsyncClient
 	}
 })();
