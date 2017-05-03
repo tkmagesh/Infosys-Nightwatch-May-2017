62a4946695cea0a325efd5567b4a244662d1da80
Magesh <tkmagesh77@gmail.com>
2017-05-03 15:50:24 +0530

    Async using callback

--- Day-01/01-JavaScript/async.js
+++ Day-01/01-JavaScript/async.js
@@ -12,19 +12,21 @@ var app = (function(){
 		console.log('[@Client] result = ', result);
 	}
 
-	function addAsync(x,y){
+	function addAsync(x,y, onResult){
 		console.log('	[@Service] processing ', x , ' and ', y);
 		setTimeout(function(){
 			var result = x + y;
 			console.log('	[@Service] returing result ');
-			return result;
+			if (typeof onResult === 'function')
+				onResult(result);
 		}, 5000);
 	}
 
 	function addAsyncClient(x,y){
 		console.log('[@Client] triggering addAsync');
-		var result = addAsync(x,y);
-		console.log('[@Client] result = ', result);
+		addAsync(x,y, function(result){
+			console.log('[@Client] result = ', result);
+		});
 	}
 
 	return {
